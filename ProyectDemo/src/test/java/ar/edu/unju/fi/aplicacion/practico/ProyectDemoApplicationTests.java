f4b96efeea89f381cb1580fbeed3c5f588d73dd8
KuruKLG <kuru_gutierrez@hotmail.com>
2022-04-10 22:07:25 -0300

    commit master

---
+++ ProyectDemo/src/test/java/ar/edu/unju/fi/aplicacion/practico/ProyectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.aplicacion.practico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
